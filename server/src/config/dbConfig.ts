029972a954cd1d99d5910db2615d0e20eaa3b976
Theo Fornetti <Theofornetti2001@gmail.com>
2023-04-26 11:19:16 -0300

    DBCONFIG

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
-// import dotenv from 'dotenv';
+import { config } from "dotenv";
+config(); 
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     port: 3306,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     // entities: [],
