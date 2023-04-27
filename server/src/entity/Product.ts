684b3f51a7ddc081c56255d3352073cf0493062b
Theo Fornetti <Theofornetti2001@gmail.com>
2023-04-27 10:27:47 -0300

    Relacion Producto

--- server/src/entity/Product.ts
+++ server/src/entity/Product.ts
@@ -1,6 +1,7 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity, ManyToOne, ManyToMany, JoinTable } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity, ManyToOne, ManyToMany, JoinTable, OneToMany } from "typeorm";
 import { User } from "./User";
 import { ProductType } from "./ProductType";
+import { OrderProduct } from "./OrderProduct";
 
 @Entity()
 export class Product extends BaseEntity {
@@ -30,6 +31,9 @@ export class Product extends BaseEntity {
 
     @ManyToOne(() => User, (user) => user.address)
     user: User
+
+    @OneToMany(() => OrderProduct, (orderproducts) => orderproducts.product)
+    orderproducts: OrderProduct[]
 
     @ManyToMany(() => ProductType)
     @JoinTable()
