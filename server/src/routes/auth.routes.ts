77cf03fa207fea1ceba2190d58e3ad1630258fb2
Theo Fornetti <Theofornetti2001@gmail.com>
2023-04-28 09:44:45 -0300

    middleware

---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import {Router} from "express"
+// import { authController } from "../controller/auth.controller"
+
+const registerRoutes = Router()
+
+// registerRoutes.post("/",  authController)
+
+
+export default registerRoutes
